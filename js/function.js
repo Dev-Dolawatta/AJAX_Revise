25d7b2c782af44a7ea2da717658ceb850add580c
Dev-Dolawatta <.dtdolawatta@gmail.com>
2025-08-02 13:27:47 +0530

    sending an ajax request and append the request's result to a h1 tag

--- js/function.js
+++ js/function.js
@@ -1,7 +1,23 @@
-$("#sendBtn").click(function(){
-    console.log('clicked!');
-    var httpObject = new XMLHttpRequest();
-    httpObject.open("GET","db/database.txt");//initialize the request
-    httpObject.send();//finalize the request
+//----------------part one-------------------
+    // $("#sendBtn").click(function(){
+    //     console.log('clicked!');
+    // var httpObject = new XMLHttpRequest();
+    // console.log(httpObject.readyState)
+    // httpObject.open("GET","db/database.txt");//initialize the request
+    // console.log(httpObject.readyState)
+    // httpObject.send();//finalize the request
+    // console.log(httpObject.readyState)
+
+    //----------------part two-----------------------
+    $("#sendBtn").click(function(){
+        console.log('clicked!');
+        $.ajax({
+           url:"db/database.txt",
+           method:"GET",
+           success:function(response){
+               console.log('function working test!')
+               $("#responseTxt").text(response);
+           }
+        });
 
 });
